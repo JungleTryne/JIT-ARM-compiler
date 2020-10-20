e8310bee65633eb28f436cf119de2fb5440778f3
Mishin Danila <danila.mishin.2001@gmail.com>
2020-10-21 00:12:37 +0300

    Generating assembler instructions. Not tested

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 #include <algorithm>
 #include <cassert>
+#include <iostream>
+#include <iterator>
 #include <memory>
 #include <optional>
 #include <tuple>
@@ -15,32 +17,34 @@
 using str_iter = std::string::iterator;
 using str_iter_const = std::string::const_iterator;
 
+class ARM_JIT_Compiler;
 /* ExpressionParser class
  * This class converts the given expression into tree
  * which can be used for ARM code generation
  */
 
+enum class ExpressionType {
+    Constant,
+    Default,
+    Variable,
+    Plus,
+    Minus,
+    Product,
+    Function
+};
+
+struct Node {
+    ExpressionType type = ExpressionType::Default;
+    size_t order_number = 0;
+    std::optional<std::string> content;
+    std::vector<std::unique_ptr<Node>> sub_expressions = {};
+};
+
 class ExpressionParser {
 public:
     explicit ExpressionParser(std::string expression);
+    friend void TransferParsingTree(ExpressionParser& parser, ARM_JIT_Compiler& compiler);
 private:
-    enum class ExpressionType {
-        Constant,
-        Default,
-        Variable,
-        Plus,
-        Minus,
-        Product,
-        Function
-    };
-
-    struct Node {
-        ExpressionType type = ExpressionType::Default;
-        size_t order_number = 0;
-        std::optional<std::string> content;
-        std::vector<std::unique_ptr<Node>> sub_expressions = {};
-    };
-
     std::string expression_;
     std::unique_ptr<Node> root_;
 
@@ -92,12 +96,12 @@ bool ExpressionParser::IsParBalance(str_iter left, str_iter right) {
 }
 
 /* Gives priority of specific operation */
-size_t ExpressionParser::GetPriority(ExpressionParser::ExpressionType operation) {
+size_t ExpressionParser::GetPriority(ExpressionType operation) {
     switch (operation) {
-        case ExpressionParser::ExpressionType::Plus:
-        case ExpressionParser::ExpressionType::Minus:
+        case ExpressionType::Plus:
+        case ExpressionType::Minus:
             return 0;
-        case ExpressionParser::ExpressionType::Product:
+        case ExpressionType::Product:
             return 1;
         default:
             return 2;
@@ -276,11 +280,414 @@ auto ExpressionParser::GetFunctionParameters(str_iter left, str_iter right) cons
         ++current_right;
         current_left = current_right;
     }
-
     assert(false);
 }
 
+class ARM_JIT_Compiler {
+public:
+    friend void TransferParsingTree(ExpressionParser& parser, ARM_JIT_Compiler& compiler);
+    void compile();
+
+    template<typename OutputIterator>
+    void print_assembly(OutputIterator& output);
+private:
+
+    enum class ARM_INSTRUCTION {
+        ADD,            //r0 += r1
+        SUB,            //r0 -= r1
+        MUL,            //r0 *= r1
+
+        BLX,            //blx *function*
+
+        LDR_FROM_NEXT,  //ldr r_i [pc, #4]
+        LDR_REG,        //reading from address in register (Example: ldr r_i, [r_j])
+
+        PUSH_MULT_REG,  //pushing several registers
+        PUSH_REG,       //pushing register
+
+        POP_MULT_REG,   //popping several registers
+        POP_REG,        //popping register
+
+        WORD_DECL,      //.word declaration
+    };
+
+    enum class ARM_REGISTER {
+        R0 = 0,
+        R1 = 1,
+        R2 = 2,
+        R3 = 3,
+        R4 = 4,
+        LR, PC
+    };
+    using ARM_R = ARM_REGISTER;
+
+    using ARM_I = ARM_INSTRUCTION;
+
+    using instruction_t = std::tuple<ARM_INSTRUCTION,
+        std::optional<ARM_REGISTER>,
+        std::optional<ARM_REGISTER>,
+        std::optional<std::string>>;
+    std::vector<instruction_t> instructions_;
+    std::unique_ptr<Node> parse_tree_;
+
+    void compile_(Node* current);
+    void handle_const(Node* current);
+    void handle_variable(Node* current);
+    void handle_plus(Node* current);
+    void handle_minus(Node* current);
+    void handle_product(Node* current);
+
+    void handle_function(Node* current);
+};
+
+void TransferParsingTree(ExpressionParser& parser, ARM_JIT_Compiler& compiler) {
+    compiler.parse_tree_ = std::move(parser.root_);
+}
+
+void ARM_JIT_Compiler::compile() {
+    compile_(parse_tree_.get());
+}
+
+void ARM_JIT_Compiler::compile_(Node *current) {
+    switch (current->type) {
+        case ExpressionType::Constant:
+            handle_const(current);
+            break;
+        case ExpressionType::Variable:
+            handle_variable(current);
+            break;
+        case ExpressionType::Plus:
+            handle_plus(current);
+            break;
+        case ExpressionType::Minus:
+            handle_minus(current);
+            break;
+        case ExpressionType::Product:
+            handle_product(current);
+            break;
+        case ExpressionType::Function:
+            handle_function(current);
+            break;
+        case ExpressionType::Default:
+            assert(false);
+    }
+}
+
+void ARM_JIT_Compiler::handle_const(Node *current) {
+    /* Handling Constant Type
+     * ARM instructions for that:
+     *
+     * ldr r0, [pc, #4]
+     * .word 0x05
+     * push {r0}
+     *
+     * P.S. 0x05 is given for example
+     */
+
+    instructions_.emplace_back ( //ldr r0, [pc, #4]
+            ARM_I::LDR_FROM_NEXT,
+            ARM_R::R0,
+            std::nullopt,
+            std::nullopt
+    );
+
+    instructions_.emplace_back ( //.word *constant*
+            ARM_I::WORD_DECL,
+            std::nullopt,
+            std::nullopt,
+            current->content
+    );
+
+    instructions_.emplace_back ( // push {r0}
+            ARM_I::PUSH_REG,
+            ARM_R::R0,
+            std::nullopt,
+            std::nullopt
+    );
+}
+
+void ARM_JIT_Compiler::handle_variable(Node *current) {
+    /* Handling Variable Type
+     * ARM instructions for that:
+     *
+     * ldr r0, [pc, #4]
+     * .word 0xfb1cfcd0
+     * ldr r0, [r0]
+     * push {r0}
+     *
+     * P.S. 0xfb1cfcd0 is given for example
+     */
+
+    instructions_.emplace_back ( //ldr r0, [pc, #4]
+            ARM_I::LDR_FROM_NEXT,
+            ARM_R::R0,
+            std::nullopt,
+            std::nullopt
+    );
+
+    instructions_.emplace_back ( //.word *constant*
+            ARM_I::WORD_DECL,
+            std::nullopt,
+            std::nullopt,
+            current->content
+    );
+
+    instructions_.emplace_back ( //ldr r0, [r0]
+            ARM_I::LDR_REG,
+            ARM_R::R0,
+            ARM_R::R0,
+            std::nullopt
+    );
+
+    instructions_.emplace_back ( // push {r0}
+            ARM_I::PUSH_REG,
+            ARM_R::R0,
+            std::nullopt,
+            std::nullopt
+    );
+}
+
+void ARM_JIT_Compiler::handle_plus(Node *current) {
+    /* Handling Plus operation
+     * ARM instructions for that:
+     *
+     * pop {r0-r1}
+     * add r0, r1, r0
+     * push {r0}
+     */
+    compile_(current->sub_expressions[0].get());
+    compile_(current->sub_expressions[1].get());
+
+    instructions_.emplace_back ( //pop {r0-r1}
+            ARM_I::POP_MULT_REG,
+            ARM_R::R0,
+            ARM_R::R1,
+            std::nullopt
+    );
+
+    instructions_.emplace_back( //add r0, r1, r0
+            ARM_I::ADD,
+            ARM_R::R0,
+            ARM_R::R1,
+            std::nullopt
+    );
+
+    instructions_.emplace_back( //push {r0}
+            ARM_I::PUSH_REG,
+            ARM_R::R0,
+            std::nullopt,
+            std::nullopt
+    );
+}
+
+void ARM_JIT_Compiler::handle_minus(Node *current) {
+    /* Handling Minus operation
+     * ARM instructions for that:
+     *
+     * pop {r0-r1}
+     * sub r0, r1, r0
+     * push {r0}
+     */
+    compile_(current->sub_expressions[0].get());
+    compile_(current->sub_expressions[1].get());
+
+    instructions_.emplace_back ( //pop {r0-r1}
+            ARM_I::POP_MULT_REG,
+            ARM_R::R0,
+            ARM_R::R1,
+            std::nullopt
+    );
+
+    instructions_.emplace_back( //sub r0, r1, r0
+            ARM_I::SUB,
+            ARM_R::R0,
+            ARM_R::R1,
+            std::nullopt
+    );
+
+    instructions_.emplace_back( //push {r0}
+            ARM_I::PUSH_REG,
+            ARM_R::R0,
+            std::nullopt,
+            std::nullopt
+    );
+}
+
+void ARM_JIT_Compiler::handle_product(Node *current) {
+    /* Handling Product operation
+     * ARM instructions for that:
+     *
+     * pop {r0-r1}
+     * mul r0, r1, r0
+     * push {r0}
+     */
+
+    compile_(current->sub_expressions[0].get());
+    compile_(current->sub_expressions[1].get());
+
+    instructions_.emplace_back ( //pop {r0-r1}
+            ARM_I::POP_MULT_REG,
+            ARM_R::R0,
+            ARM_R::R1,
+            std::nullopt
+    );
+
+    instructions_.emplace_back( //mul r0, r1, r0
+            ARM_I::MUL,
+            ARM_R::R0,
+            ARM_R::R1,
+            std::nullopt
+    );
+
+    instructions_.emplace_back( //push {r0}
+            ARM_I::PUSH_REG,
+            ARM_R::R0,
+            std::nullopt,
+            std::nullopt
+    );
+}
+
+void ARM_JIT_Compiler::handle_function(Node *current) {
+    /* Handling Function call
+     * ARM instructions for that:
+     *
+     * pop {r0-ri}
+     * ldr r4, [pc, #4]
+     * .word 0xfb1cfcd0
+     * blx r4
+     * push {r0}
+     */
+
+    std::for_each(current->sub_expressions.begin(),
+                  current->sub_expressions.end(),
+                  [this](const std::unique_ptr<Node>& unique_pointer) {
+        compile_(unique_pointer.get());
+    });
+
+    size_t arguments_number = current->sub_expressions.size();
+    assert(arguments_number > 0);
+
+    instructions_.emplace_back ( //pop {r0-ri}
+            ARM_I::POP_MULT_REG,
+            ARM_R::R0,
+            static_cast<ARM_R>(static_cast<size_t>(ARM_R::R0) + arguments_number - 1),
+            std::nullopt
+    );
+
+    instructions_.emplace_back ( //ldr r0, [pc, #4]
+            ARM_I::LDR_FROM_NEXT,
+            ARM_R::R4,
+            std::nullopt,
+            std::nullopt
+    );
+
+    instructions_.emplace_back ( //.word 0xfb1cfcd0
+            ARM_I::WORD_DECL,
+            std::nullopt,
+            std::nullopt,
+            current->content
+    );
+
+    instructions_.emplace_back(
+            ARM_I::BLX,
+            ARM_R::R4,
+            std::nullopt,
+            std::nullopt
+    );
+
+    instructions_.emplace_back ( // push {r0}
+            ARM_I::PUSH_REG,
+            ARM_R::R0,
+            std::nullopt,
+            std::nullopt
+    );
+}
+
+template<typename OutputIterator>
+void ARM_JIT_Compiler::print_assembly(OutputIterator& output) {
+    std::for_each(instructions_.begin(),
+                  instructions_.end(),
+                  [this, &output](const instruction_t& instruction)
+    {
+        switch (std::get<0>(instruction)) {
+            case ARM_I::ADD:
+                *output = std::string("add\t") +
+                          "r" + std::to_string(static_cast<int>(*std::get<1>(instruction))) + ",\t" +
+                          "r" + std::to_string(static_cast<int>(*std::get<2>(instruction))) + ",\t" +
+                          "r" + std::to_string(static_cast<int>(*std::get<1>(instruction))) + "\n";
+                break;
+
+            case ARM_I::SUB:
+                *output = std::string("sub\t") +
+                          "r" + std::to_string(static_cast<int>(*std::get<1>(instruction))) + ",\t" +
+                          "r" + std::to_string(static_cast<int>(*std::get<2>(instruction))) + ",\t" +
+                          "r" + std::to_string(static_cast<int>(*std::get<1>(instruction))) + "\n";
+                break;
+
+            case ARM_I::MUL:
+                *output = std::string("mul\t") +
+                          "r" + std::to_string(static_cast<int>(*std::get<1>(instruction))) + ",\t" +
+                          "r" + std::to_string(static_cast<int>(*std::get<2>(instruction))) + ",\t" +
+                          "r" + std::to_string(static_cast<int>(*std::get<1>(instruction))) + "\n";
+                break;
+
+            case ARM_I::BLX:
+                *output = std::string("blx\t") +
+                          "r" + std::to_string(static_cast<int>(*std::get<1>(instruction))) + "\n";
+                break;
+
+            case ARM_I::LDR_FROM_NEXT:
+                *output = std::string("ldr\t") +
+                          "r" + std::to_string(static_cast<int>(*std::get<1>(instruction))) + ",\t" +
+                          "[pc, #4]" + "\n";
+                break;
+
+            case ARM_I::LDR_REG:
+                *output = std::string("ldr\t") +
+                          "r" + std::to_string(static_cast<int>(*std::get<1>(instruction))) + ",\t" +
+                          "[" + "r" + std::to_string(static_cast<int>(*std::get<2>(instruction))) + "]" + "\n";
+                break;
+
+            case ARM_I::PUSH_REG:
+                *output = std::string("push\t{r") +
+                          std::to_string(static_cast<int>(*std::get<1>(instruction))) + "}\n";
+                break;
+
+            case ARM_I::PUSH_MULT_REG:
+                *output = std::string("push\t{r") +
+                          std::to_string(static_cast<int>(*std::get<1>(instruction))) + "-r" +
+                          std::to_string(static_cast<int>(*std::get<2>(instruction))) +
+                          + "}\n";
+                break;
+
+            case ARM_I::WORD_DECL:
+                *output = std::string(".word\t") +
+                          std::string(*std::get<3>(instruction)) + "\n";
+                break;
+
+            case ARM_I::POP_MULT_REG:
+                *output = std::string("pop\t{r") +
+                          std::to_string(static_cast<int>(*std::get<1>(instruction))) + "-r" +
+                          std::to_string(static_cast<int>(*std::get<2>(instruction))) +
+                          + "}\n";
+                break;
+
+            default:
+                *output = std::string("UNKNOWN_INSTRUCTION\n");
+                break;
+        }
+        ++output;
+    });
+}
+
+
 int main() {
     ExpressionParser parser("(1+a)*c + div(2+4,2)");
+    ARM_JIT_Compiler compiler;
+    TransferParsingTree(parser, compiler);
+    compiler.compile();
+
+    auto out_iter = std::ostream_iterator<std::string>(std::cout, "");
+    compiler.print_assembly(out_iter);
     return 0;
 }
